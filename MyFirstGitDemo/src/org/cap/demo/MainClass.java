fcfc95c8f131373458141b9b2ea9e20e99855ca0
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2017-12-05 12:39:54 +0530

    SalesClass Added.

--- MyFirstGitDemo/src/org/cap/demo/MainClass.java
+++ MyFirstGitDemo/src/org/cap/demo/MainClass.java
@@ -3,7 +3,8 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+		SalesClass obj=new SalesClass();
+		obj.display();
 		greetings();
 	}
 	
---
+++ MyFirstGitDemo/src/org/cap/demo/SalesClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class SalesClass {
+	
+	int[] arr={1,2,3,4,5,-45};
+	
+	
+	public void display(){
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+
+}
