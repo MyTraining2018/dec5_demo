232420a57e96a77fef6d09f97aad81b19770b4cf
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2017-12-06 09:59:41 +0530

    enhanced loop added in display method.

--- MyFirstGitDemo/src/org/cap/demo/SalesClass.java
+++ MyFirstGitDemo/src/org/cap/demo/SalesClass.java
@@ -6,8 +6,12 @@ public class SalesClass {
 	
 	
 	public void display(){
-		for(int i=0;i<arr.length;i++)
-			System.out.println(arr[i]);
+		int sum=0;
+		for(int n:arr){
+			System.out.println(n);
+			sum+=n;
+		}
+		System.out.println("Sum:" + sum);
 	}
 	
 	public void sayHello(){
