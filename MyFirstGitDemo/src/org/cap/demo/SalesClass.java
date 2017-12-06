ce2e51b0900e33cef36f47b090009c054e5e9080
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2017-12-06 09:57:31 +0530

    display method updated.

--- MyFirstGitDemo/src/org/cap/demo/SalesClass.java
+++ MyFirstGitDemo/src/org/cap/demo/SalesClass.java
@@ -6,8 +6,11 @@ public class SalesClass {
 	
 	
 	public void display(){
-		for(int i=0;i<arr.length;i++)
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
 			System.out.println(arr[i]);
+			sum+=arr[i];
+		}
 	}
 	
 	public void sayHello(){
